d70fc39681256b09124497c39a588ff017a54f05
Takahiro H <43534055+takahiro-hanada@users.noreply.github.com>
2023-01-19 08:50:07 +0900

    Update Demo

--- src/DemoWinFormsApp/Form1.Designer.cs
+++ src/DemoWinFormsApp/Form1.Designer.cs
@@ -69,9 +69,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(21, 28);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(160, 12);
+            this.label1.Size = new System.Drawing.Size(123, 12);
             this.label1.TabIndex = 1;
-            this.label1.Text = ".NET Framework Static values";
+            this.label1.Text = ".NET Framework Static";
             // 
             // label3
             // 
